081013f9b7ed0ec1e90afa6a1f839d6471fd3f0d
musfik070-gif <musfik070@gmail.com>
2026-05-21 03:09:49 +0600

    My Idea Page Created

--- src/pages/MyIdeas.jsx
+++ src/pages/MyIdeas.jsx
@@ -1,7 +1,66 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useAuth } from "../providers/AuthProvider";
+
 const MyIdeas = () => {
+  const { user } = useAuth();
+
+  const [ideas, setIdeas] = useState([]);
+
+  useEffect(() => {
+    if (user?.email) {
+      fetchMyIdeas();
+    }
+  }, [user]);
+
+  const fetchMyIdeas = async () => {
+    try {
+      const res = await axios.get(
+        `http://localhost:5001/my-ideas?email=${user.email}`,
+      );
+
+      setIdeas(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">My Ideas Page</h1>
+    <div className="min-h-screen px-5 py-16">
+      <div className="max-w-6xl mx-auto">
+        <h2 className="text-5xl font-bold text-center mb-14">My Ideas</h2>
+
+        {ideas.length === 0 ? (
+          <h3 className="text-center text-xl">No Ideas Found</h3>
+        ) : (
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {ideas.map((idea) => (
+              <div
+                key={idea._id}
+                className="bg-white dark:bg-gray-800 rounded-3xl shadow-lg overflow-hidden"
+              >
+                <img
+                  src={idea.image}
+                  alt={idea.title}
+                  className="h-56 w-full object-cover"
+                />
+
+                <div className="p-6">
+                  <h3 className="text-2xl font-bold mb-3">{idea.title}</h3>
+
+                  <p className="text-gray-500 dark:text-gray-300 mb-4">
+                    {idea.description}
+                  </p>
+
+                  <span className="bg-blue-600 text-white px-4 py-1 rounded-full text-sm">
+                    {idea.category}
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
