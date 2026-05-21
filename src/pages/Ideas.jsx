8fac7a9a00d543cb28b66d4a68009d6eb0366f2a
musfik070-gif <musfik070@gmail.com>
2026-05-21 20:40:35 +0600

    added serch filter

--- src/pages/Ideas.jsx
+++ src/pages/Ideas.jsx
@@ -4,20 +4,47 @@ import { Link } from "react-router-dom";
 
 const Ideas = () => {
   const [ideas, setIdeas] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     axios
-      .get("http://localhost:5001/ideas")
+      .get(`http://localhost:5001/ideas?search=${search}&filter=${filter}`)
       .then((res) => {
         setIdeas(res.data);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [search, filter]);
 
   return (
     <div className="min-h-screen bg-[#020817] text-white p-10">
       <h1 className="text-5xl font-bold text-center mb-10">All Ideas</h1>
 
+      
+      <div className="flex flex-col md:flex-row justify-center gap-4 mb-10 max-w-2xl mx-auto">
+        <input
+          type="text"
+          placeholder="Search by Title..."
+          value={search}
+          className="w-full md:w-2/3 px-4 py-3 rounded-lg bg-[#1e293b] text-white border border-gray-600 focus:outline-none"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <select
+          value={filter}
+          className="w-full md:w-1/3 px-4 py-3 rounded-lg bg-[#1e293b] text-white border border-gray-600 focus:outline-none"
+          onChange={(e) => setFilter(e.target.value)}
+        >
+          <option value="">All Categories</option>
+          <option value="AI">AI</option>
+          <option value="Health">Health</option>
+          <option value="Education">Education</option>
+          <option value="FinTech">FinTech</option>
+          <option value="E-Commerce">E-Commerce</option>
+        </select>
+      </div>
+      
+      
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
         {ideas.map((idea) => (
           <div key={idea._id} className="bg-[#1e293b] p-5 rounded-xl">
